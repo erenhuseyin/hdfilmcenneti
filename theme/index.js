8a138aa54aca07064cfdc70dc786656e049aadbe
huseyineren <huseyin.eren@finastech.com>
2023-11-23 16:48:57 +0300

    movie-app

--- theme/index.js
+++ theme/index.js
@@ -2,7 +2,6 @@ export const theme = {
     background: '#eab308',
     text: '#eab308'
 }
-
 export const styles = {
     text: {color: theme.text},
     background: {backgroundColor: theme.background}
